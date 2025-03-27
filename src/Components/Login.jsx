44ff0dbea4b2a023923a626856bcc8c845d14b9e
Pavitrank <pavitrank2002@gmail.com>
2025-03-27 20:10:02 +0530

    feat: Implemented the login functionality andreceived the token. Added new package as React Router

---
+++ src/Components/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [loginID, setLoginID] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [token, setToken] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("https://reqres.in/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: loginID, password: loginPassword }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        console.log("Failed to login the user");
+      } else {
+        console.log("Login successful: " + data.token);
+        console.log(data);
+        setToken(data.token);
+      }
+
+      console.log(response);
+      navigate("/users");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <section>
+        <h1>Welcome to Login screen</h1>
+        <h2>Email: </h2>
+        <input
+          type="text"
+          placeholder="Enter the email"
+          value={loginID}
+          onChange={(e) => setLoginID(e.target.value)}
+        />
+        <h2>Password: </h2>
+        <input
+          type="password"
+          placeholder="Enter the password"
+          value={loginPassword}
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login Button</button>
+      </section>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/Components/UserList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserList = () => {
+  return <section>This is the user list page.</section>;
+};
+
+export default UserList;
