567be30b04f1f29bd9384453a1ca61859f077105
Pavitrank <pavitrank2002@gmail.com>
2025-03-27 20:28:22 +0530

    feat: Implemented user fetching from the api and displayed the data in the form of list

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const UserList = () => {
-  return <section>This is the user list page.</section>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        if (!response.ok) {
+          throw new console.error("Network response was not ok");
+        }
+        const data = await response.json();
+        setUsers(data.data);
+        console.log(data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+  return (
+    <section>
+      {users.map((user) => (
+        <ul key={user.id}>
+          <li>{user.email}</li>
+          <li>{user.first_name}</li>
+          <li>{user.last_name}</li>
+          <li>{user.avatar}</li>
+        </ul>
+      ))}
+    </section>
+  );
 };
 
 export default UserList;
